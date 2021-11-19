cea674455dd4535216d2c51c1377f475934e5329
Alessandro Varela <alessandrovarela@gmail.com>
2021-11-19 06:20:32 +0000

    add devise and welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+  get 'inicio', to:'welcome#index'
+
+  devise_for :profiles
+  devise_for :admins
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to:'welcome#index'
 end
