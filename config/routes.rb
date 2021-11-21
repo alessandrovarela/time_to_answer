1da93d90d67f130185802a2c4daa66ff5e524d0f
Alessandro Varela <alessandrovarela@gmail.com>
2021-11-21 06:21:12 +0000

    replace controllers profiles to user

--- config/routes.rb
+++ config/routes.rb
@@ -2,19 +2,19 @@ Rails.application.routes.draw do
   namespace :site do
     get 'welcome/index'
   end
-
-  namespace :profiles_backoffice do
+  
+  namespace :users_backoffice do
     get 'welcome/index'
   end
-
+  
   namespace :admins_backoffice do
     get 'welcome/index'
   end
-
+  
   get 'inicio', to:'site/welcome#index'
-
-  devise_for :profiles
+  
   devise_for :admins
+  devise_for :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to:'site/welcome#index'
