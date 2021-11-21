ad4e0442fcb67276ca242549ad5d09d3563ba8ad
Alessandro Varela <alessandrovarela@gmail.com>
2021-11-21 04:00:11 +0000

    site layout adjustment

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  namespace :site do
+    get 'welcome/index'
+  end
+
   namespace :profiles_backoffice do
     get 'welcome/index'
   end
@@ -7,12 +11,11 @@ Rails.application.routes.draw do
     get 'welcome/index'
   end
 
-  get 'welcome/index'
-  get 'inicio', to:'welcome#index'
+  get 'inicio', to:'site/welcome#index'
 
   devise_for :profiles
   devise_for :admins
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to:'welcome#index'
+  root to:'site/welcome#index'
 end
